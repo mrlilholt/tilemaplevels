cc17c1c39b3a02846f7068d63a777081d54c5303
mrlilholt <63080594+mrlilholt@users.noreply.github.com>
2020-04-28 13:18:40 -0400

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const trailer = SpriteKind.create()
+}
 function buildLevel () {
     if (nextLevel == levels.length) {
         game.over(true)
@@ -43,22 +46,22 @@ f f f f f f f f f f
 3 3 3 3 3 3 3 3 3 3 
 `]
 scene.setTile(7, img`
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
-7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
-7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
-7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
-7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 f 7 7 7 7 7 7 7 7 f f 7 7 7 
-7 7 7 f f 7 7 7 7 7 7 f 7 7 7 7 
-7 7 7 7 f f 7 7 7 7 f f 7 7 7 7 
-7 7 7 7 7 f f f f f f 7 7 7 7 7 
-7 7 7 7 7 7 7 f f 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b f b b b b f b b b b b b 
+b b b b f b b b b f b b b b b b 
+b b b b f b b b b f b b b b b b 
+b b b b f b b b b f b b b b b b 
+b b b b f b b b b f b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b f b b b b b b b b f f b b b 
+b b b f f b b b b b b f b b b b 
+b b b b f f b b b b f f b b b b 
+b b b b b f f f f f f b b b b b 
+b b b b b b b f f b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
 `, true)
 nextLevel = 0
 player = sprites.create(img`
@@ -79,5 +82,30 @@ player = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
+let trail = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+5 5 5 5 5 5 5 5 5 5 . . . . . . 
+5 5 5 5 5 5 5 5 5 5 5 . . . . . 
+3 3 3 5 5 5 5 5 5 5 5 5 5 5 5 5 
+3 3 3 3 3 3 3 3 3 5 5 5 5 5 5 5 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 9 
+7 7 7 7 7 7 7 7 7 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.trailer)
+trail.follow(player, 100)
+trail.setPosition(7, 47)
 controller.moveSprite(player, 100, 100)
 buildLevel()
+forever(function () {
+    if (player.overlapsWith(trail)) {
+        trail.x += -5
+    }
+})
